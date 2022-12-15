ab013627aeb4f1b5ad8db607abcb1e8c2788a901
Sepja <r.labuhn1990@gmail.com>
2022-12-15 08:52:25 +0100

    overhauled main_push.yaml

--- .github/actions/freeze/index.js
+++ .github/actions/freeze/index.js
@@ -9,7 +9,7 @@ try {
     console.log(`Valid ${valid}!`);
     const time = (new Date()).toTimeString();
     core.setOutput("time", time);
-    core.setOutput("isValid", valid);
+    core.setOutput("isValid", true);
     // Get the JSON webhook payload for the event that triggered the workflow
 } catch (error) {
     core.setFailed(error.message);
